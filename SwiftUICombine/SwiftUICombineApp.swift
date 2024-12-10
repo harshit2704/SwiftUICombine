5bbce717d2e9f4dac223ff6f0056e84af59330fa
harshit <harsh.rastogi2704@gmail.com>
2024-12-10 22:07:18 +0530

    Initial Commit

---
+++ SwiftUICombine/SwiftUICombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineApp.swift
+//  SwiftUICombine
+//
+//  Created by Harshit Rastogi on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
